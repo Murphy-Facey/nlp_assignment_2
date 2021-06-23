ecb84ce87d5cf15e564ed4b5f99db7892abfb964
BarcaJames <jamie1k_86@yahoo.com>
2021-06-22 20:52:54 -0400

    Added front end features

--- client/src/components/Tokens/Tokens.jsx
+++ client/src/components/Tokens/Tokens.jsx
@@ -1,13 +1,16 @@
 import { nanoid } from "nanoid";
 import Word from "../Word/Word";
 
-const Tokens = ({ tokens, class_name, name }) => {
+const Tokens = ({ tokens, class_name, name, displayTokenInfo }) => {
+
   return (
     <div className={class_name}>
       <h3>{name}</h3>
       <div className="words">
         {tokens.map((token) => (
-          <Word key={nanoid()} text={token.text} number={token.index} />
+          <div key={nanoid()} onClick={() => displayTokenInfo(token.index)}>
+            <Word text={token.text} number={token.index} />
+          </div>
         ))}
       </div>
     </div>
